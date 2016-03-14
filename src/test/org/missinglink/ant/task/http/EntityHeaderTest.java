107ac2538a60e69a87554434caf8a77168952e93
Aaron Meriwether <me@awirtz.com>
2016-03-14 13:27:00 -0600

    Add print-headers tests
    Tests to validate issue #30

--- src/test/org/missinglink/ant/task/http/EntityHeaderTest.java
+++ src/test/org/missinglink/ant/task/http/EntityHeaderTest.java
@@ -206,52 +206,99 @@ package org.missinglink.ant.task.http;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItem;
-import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.util.List;
 
-import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-public abstract class AbstractHttpTaskTest extends AbstractAntTest {
+public class EntityHeaderTest extends AbstractAntTest {
 
-  public AbstractHttpTaskTest(final String tasksxml) throws IOException {
-    super(tasksxml);
+  public EntityHeaderTest() throws IOException {
+    super("<target name=\"headers_get\">\n" +
+        "  <http url=\"${server_uri}${server_context}\" method=\"GET\">\n" +
+        "    <headers>\n" +
+        "      <header name=\"${header_name1}\" value=\"${header_value1}\" />\n" +
+        "      <header name=\"${header_name2}\" value=\"${header_value2}\" />\n" +
+        "    </headers>\n" +
+        "  </http>\n" +
+        "</target>\n" +
+        "<target name=\"silent_request_headers\">\n" +
+        "  <http url=\"${server_uri}${server_context}\" method=\"GET\" printRequestHeaders=\"false\">\n" +
+        "    <headers>\n" +
+        "      <header name=\"${header_name1}\" value=\"${header_value1}\" />\n" +
+        "      <header name=\"${header_name2}\" value=\"${header_value2}\" />\n" +
+        "    </headers>\n" +
+        "  </http>\n" +
+        "</target>\n" +
+        "<target name=\"silent_response_headers\">\n" +
+        "  <http url=\"${server_uri}${server_context}\" method=\"GET\" printResponseHeaders=\"false\">\n" +
+        "    <headers>\n" +
+        "      <header name=\"${header_name1}\" value=\"${header_value1}\" />\n" +
+        "      <header name=\"${header_name2}\" value=\"${header_value2}\" />\n" +
+        "    </headers>\n" +
+        "  </http>\n" +
+        "</target>");
+  }
+
+  @Before
+  public void before() throws Exception {
+    startHttpServer();
+    project.setNewProperty("server_uri", getHttpServerUri());
+  }
+
+  @After
+  public void after() throws IOException {
+    stopHttpServer();
   }
 
   @Test
-  public void testSimpleGet200() {
-    project.setProperty("server_context", PING_CONTEXT);
-    final List<String> taskLog = buildRule.logExecuteTarget("simple_get", Project.MSG_DEBUG);
+  public void testHeadersGet() {
+    project.setProperty("server_context", ECHO_HEADERS_CONTEXT);
+    project.setProperty("header_name1", "Content-Type");
+    project.setProperty("header_value1", "text/xml");
+    project.setProperty("header_name2", "Accept");
+    project.setProperty("header_value2", "text/plain, image/png");
+    final List<String> taskLog = buildRule.logExecuteTarget("headers_get", Project.MSG_DEBUG);
 
-    assertThat(taskLog, hasItem(startsWith("[http] HTTP GET ")));
-    assertThat(taskLog, hasItem(equalTo("[http] Status:\t\t200")));
+    assertThat(taskLog, hasItem(equalTo("[http] \tContent-Type: text/xml")));
+    assertThat(taskLog, hasItem(equalTo("[http] \tAccept: text/plain, image/png")));
+    assertThat(taskLog, hasItem(equalTo("[http] \tX-Req-Content-type: text/xml")));
+    assertThat(taskLog, hasItem(equalTo("[http] \tX-Req-Accept: text/plain, image/png")));
   }
 
   @Test
-  public void testSimpleGet404() {
-    project.setProperty("server_context", "/doesnt/exist");
-    try {
-      buildRule.executeTarget("simple_get");
-      fail("Target should have thrown a BuildException");
-    } catch (final BuildException ex) {
-      assertThat(ex.getMessage(), startsWith("Expected Status [200] but got [404] for URI"));
-    }
+  public void testSilentRequestHeaders() {
+    project.setProperty("server_context", ECHO_HEADERS_CONTEXT);
+    project.setProperty("header_name1", "Content-Type");
+    project.setProperty("header_value1", "text/xml");
+    project.setProperty("header_name2", "Accept");
+    project.setProperty("header_value2", "text/plain, image/png");
+    final List<String> taskLog = buildRule.logExecuteTarget("headers_get", Project.MSG_DEBUG);
+
+    assertThat(taskLog, not(hasItem(equalTo("[http] \tContent-Type: text/xml"))));
+    assertThat(taskLog, not(hasItem(equalTo("[http] \tAccept: text/plain, image/png"))));
+    assertThat(taskLog, hasItem(equalTo("[http] \tX-Req-Content-type: text/xml")));
+    assertThat(taskLog, hasItem(equalTo("[http] \tX-Req-Accept: text/plain, image/png")));
   }
 
   @Test
-  public void testSimpleGet500() {
-    project.setProperty("server_context", INTERNAL_SERVER_ERROR_CONTEXT);
-    try {
-      buildRule.executeTarget("simple_get");
-      fail("Target should have thrown a BuildException");
-    } catch (final BuildException ex) {
-      assertThat(ex.getMessage(), startsWith("Expected Status [200] but got [500] for URI"));
-    }
-  }
+  public void testSilentResponseHeaders() {
+    project.setProperty("server_context", ECHO_HEADERS_CONTEXT);
+    project.setProperty("header_name1", "Content-Type");
+    project.setProperty("header_value1", "text/xml");
+    project.setProperty("header_name2", "Accept");
+    project.setProperty("header_value2", "text/plain, image/png");
+    final List<String> taskLog = buildRule.logExecuteTarget("headers_get", Project.MSG_DEBUG);
 
+    assertThat(taskLog, hasItem(equalTo("[http] \tContent-Type: text/xml")));
+    assertThat(taskLog, hasItem(equalTo("[http] \tAccept: text/plain, image/png")));
+    assertThat(taskLog, not(hasItem(equalTo("[http] \tX-Req-Content-type: text/xml"))));
+    assertThat(taskLog, not(hasItem(equalTo("[http] \tX-Req-Accept: text/plain, image/png"))));
+  }
 }
